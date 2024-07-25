cb604ad4e21cb05d5f5cc9328078404323e256e9
Bayar Ali <136832063+BayarAli2002@users.noreply.github.com>
2024-07-25 17:37:27 +0300

    info

---
+++ info.dart
@@ -0,0 +1,9 @@
+class info {
+  //this is not a Widget it's a class of type info
+  //here String? means that the variable can be null optional parameter
+  String name;
+  int height;
+  DateTime date;
+
+  info({required this.name, required this.height, required this.date});
+}
